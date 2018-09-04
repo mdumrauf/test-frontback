57b7d6ac86a9a9dc2ba1d47d9dc5b26512daa28d
Matias Dumrauf <matias.dumrauf@gmail.com>
2018-09-04 12:23:03 -0300

    recursively render sectors

--- front/src/App.js
+++ front/src/App.js
@@ -2,7 +2,26 @@ import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import sectors from './sectors';
+
 class App extends Component {
+
+    constructor(props) {
+        super(props);
+        this.renderSector = this.renderSector.bind(this);
+    }
+
+    renderSector(sector) {
+        return (
+            <ul>
+                <li key={sector.id}>{sector.name}</li>
+                {
+                    sector.items && sector.items.map(item => this.renderSector(item))
+                }
+            </ul>
+        );
+    }
+
     render() {
         return (
             <div className="App">
@@ -10,9 +29,11 @@ class App extends Component {
                     <img src={logo} className="App-logo" alt="logo"/>
                     <h1 className="App-title">Welcome to React</h1>
                 </header>
-                <p className="App-intro">
-                    To get started, edit <code>src/App.js</code> and save to reload.
-                </p>
+                <div className="App-intro">
+                    {
+                        sectors.map(sector => this.renderSector(sector))
+                    }
+                </div>
             </div>
         );
     }
